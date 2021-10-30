f5f545173e75efe65b2c473b724d4ef557d2cc74
princeniyonshuti <npprince47@gmail.com>
2021-10-30 16:15:50 +0200

    added map on itenary list

--- src/Components/Itenaries.js
+++ src/Components/Itenaries.js
@@ -3,16 +3,17 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-function Itenaries() {
+function Itenaries(props) {
+	const { id, title, day, time } = props.itenary;
 	return (
 		<div>
 			<li className="flex justify-between items-center bg-gray-200 mt-2 p-2 cursor-pointer transition">
 				<div className="flex ml-2">
 					<div className="flex flex-col ml-2">
-						<Link to={`/itenary/${5}`}>
-							<h1 className="font-bold text-xl">Read React Documentation</h1>
+						<Link to={`/itenary/${id}`}>
+							<h1 className="font-bold text-xl">{title}</h1>
 						</Link>
-						<p className="font-normal">Sun at 10AM</p>
+						<p className="font-normal">{day} at {time}</p>
 					</div>
 				</div>
 				<div className="flex flex-col items-center">
--- src/Layout/Main.js
+++ src/Layout/Main.js
@@ -1,13 +1,27 @@
 /** @format */
 
-import React from "react";
+import React, { useState } from "react";
 import Itenaries from "../Components/Itenaries";
 function Main() {
+	const itenaryList = [
+		{
+			id: 1,
+			title: "Read React Documentation",
+			day: "Sunday",
+			time: "10Am",
+			description:
+				"Proin eget tortor risus. Pellentesque in ipsum id orci porta dapibus.",
+		},
+	];
+
+	const [itenaryData, setItenaryData] = useState(itenaryList);
 	return (
 		<div>
 			<div className="w-full p-4">
 				<ul>
-					<Itenaries />
+					{itenaryData.map((itenary) => (
+						<Itenaries key={itenary.id} itenary={itenary} />
+					))}
 				</ul>
 			</div>
 		</div>
