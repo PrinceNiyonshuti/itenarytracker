8995d675cb81ea3c342d8b46c09dae25e2db7037
princeniyonshuti <npprince47@gmail.com>
2021-10-30 15:16:26 +0200

    added routes and pages

---
+++ src/Layout/AddItenary.js
@@ -0,0 +1,44 @@
+/** @format */
+
+import React from "react";
+
+function AddItenary() {
+	return (
+		<div>
+			<h3 className="font-bold text-lg mb-1 mx-5">New Itenary</h3>
+			<form>
+				<div className="flex">
+					<div className="w-full bg-white p-2 rounded">
+						<div className=" p-3 w-full">
+							<input
+								type="text"
+								required
+								id="name"
+								className="border p-2 rounded w-full"
+								placeholder="Itenary Name"
+							/>
+						</div>
+						<div className=" p-3 w-full">
+							<textarea
+								rows="3"
+								id="name"
+								className="border p-2 rounded w-full"
+								placeholder="Write something..."
+							/>
+						</div>
+
+						<div className="flex justify-between mx-3">
+							<div>
+								<button className="px-4 py-1 bg-blue-800 text-white rounded font-light hover:bg-blue-700">
+									Post Comment
+								</button>
+							</div>
+						</div>
+					</div>
+				</div>
+			</form>
+		</div>
+	);
+}
+
+export default AddItenary;
--- src/Layout/Layout.js
+++ src/Layout/Layout.js
@@ -1,19 +1,31 @@
 /** @format */
 
 import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 import Title from "./Title";
 import Main from "./Main";
+import AddItenary from "./AddItenary";
 const Layout = () => {
 	return (
-		<div className="">
-			<div className="flex items-center justify-center w-screen mt-10 ">
-				<div className="w-2/4 font-bold border shadow-lg p-4">
-					<Title />
-					<Main/>
+		<Router>
+			<div className="">
+				<div className="flex items-center justify-center w-screen mt-10 ">
+					<div className="w-2/4 font-bold border shadow-lg p-4">
+						<Title />
+
+						<Switch>
+							<Route exact path="/">
+								<Main />
+							</Route>
+							<Route  path="/add">
+								<AddItenary />
+							</Route>
+						</Switch>
+					</div>
 				</div>
 			</div>
-		</div>
+		</Router>
 	);
 };
 export default Layout;
